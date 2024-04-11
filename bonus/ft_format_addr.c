77be2a14f7431adf2b0a12edc7c5e1a525827ffc
Matthew Yeow <myeow@u91z06s03.42kl.edu.my>
2024-04-11 22:23:21 +0800

    Fixed pointer=0 when precision=0 case

--- bonus/ft_format_addr.c
+++ bonus/ft_format_addr.c
@@ -6,7 +6,7 @@
 /*   By: myeow <myeow@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 13:53:01 by myeow             #+#    #+#             */
-/*   Updated: 2024/03/14 19:37:28 by myeow            ###   ########.fr       */
+/*   Updated: 2024/04/11 22:19:45 by myeow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void	ft_format_addr(t_data *data)
 
 static void	ft_appendaddr_buffer(t_data *data, uintptr_t n, char *base)
 {
+	if (!n && !data->flags.precision)
+		return ;
 	if (n < 16)
 		ft_appendchar_buffer(data, base[n], 1);
 	else
@@ -53,7 +55,7 @@ int	ft_printaddr(t_data *data, uintptr_t n, char *base)
 
 	w = data->flags.width - 2;
 	p = data->flags.precision;
-	nlen = ft_addrlen(n);
+	nlen = ft_addrlen(n) - (!data->flags.precision * ft_addrlen(n));
 	p = nlen + ((p >= nlen) * (p - nlen));
 	if (data->flags.hyphen)
 	{
